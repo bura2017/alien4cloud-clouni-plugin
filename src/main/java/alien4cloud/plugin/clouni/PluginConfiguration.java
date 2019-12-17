9bcfb36b5bfad4765c1bfc42ffdf044f3089f190
bura2017 <valeriya.shvetcova@phystech.edu>
2019-12-17 17:56:29 +0300

    Initial commit

---
+++ src/main/java/alien4cloud/plugin/clouni/PluginConfiguration.java
@@ -0,0 +1,12 @@
+package alien4cloud.plugin.clouni;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Valeriya Shvetcova
+ */
+@Configuration
+@ComponentScan(basePackages = "alien4cloud.plugin.clouni")
+public class PluginConfiguration {
+}
